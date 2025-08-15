ecdca447d45e27ff7b5d89f6c021efdbbf08d874
aaditya6882 <aadityaacharya6882@gmail.com>
2025-08-15 16:02:34 +0545

    feat: made front with connection and proper authentication with the backend

--- MobileBanking/src/main.jsx
+++ MobileBanking/src/main.jsx
@@ -2,8 +2,8 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-const clientId = '262901389171-e7tjssf2iirsdn3n2a85r5e7sh08nl9e.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
