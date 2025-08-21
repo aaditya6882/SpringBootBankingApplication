099c17f51904e3f91741ad946c08e63eb5647152
aaditya6882 <aadityaacharya6882@gmail.com>
2025-08-21 15:15:32 +0545

    Completed the working application

--- MobileBanking/src/components/AdminDashboard.jsx
+++ MobileBanking/src/components/AdminDashboard.jsx
@@ -3,7 +3,13 @@ import Update from "./update.jsx";
 import SearchUser from "./SearchAccount.jsx";
 import {DepositWithDraw} from "./DepositWithDraw.jsx";
 import {EnableDisableAccount} from "./EnableDisableAccount.jsx";
+import { useNavigate } from "react-router-dom";
 export default function AdminDashboard() {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        navigate("/");
+    };
     return (
         <div style={{ padding: "50px" }}>
             <h2>Admin Dashboard</h2>
@@ -12,6 +18,7 @@ export default function AdminDashboard() {
             <SearchUser/>
             <DepositWithDraw/>
             <EnableDisableAccount/>
+            <button onClick={handleLogout}>LogOut</button>
         </div>
     );
 }
--- MobileBanking/src/components/UserDashboard.jsx
+++ MobileBanking/src/components/UserDashboard.jsx
@@ -1,11 +1,20 @@
 import {Balance} from "./Balance.jsx";
 import {Transaction} from "./Transaction.jsx";
+import { useNavigate } from "react-router-dom";
+
 export default function UserDashboard() {
+        const navigate = useNavigate();
+        const handleLogout = () => {
+                localStorage.removeItem("user");
+                navigate("/");
+        };
         return (
             <div style={{ padding: "50px" }}>
                 <h2>User Dashboard</h2>
                 <Balance />
                 <Transaction />
+
+                <button onClick={handleLogout}>LogOut</button>
             </div>
         );
 }
